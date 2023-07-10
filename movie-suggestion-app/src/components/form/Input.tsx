bbce28a89786585b0a4876bef57c061d681be0a1
ozge <ozgecaylioglu@gmail.com>
2023-07-10 14:59:28 -0400

    created input component

--- movie-suggestion-app/src/components/form/Input.tsx
+++ movie-suggestion-app/src/components/form/Input.tsx
@@ -8,8 +8,8 @@ type InputProps = {
 export const Input = ({ label, labelcn, type, name, ...rest }: InputProps) => {
     return (
         <div className="input-field">
-            {label ? <label className="labelcn">{label}</label> : ''}
             <input type={type} name={name} {...rest} />
+            {label ? <label className={labelcn}>{label}</label> : ''}
         </div>
     );
 };
